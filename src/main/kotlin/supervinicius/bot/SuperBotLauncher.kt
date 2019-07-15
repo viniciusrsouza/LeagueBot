a7a007f6b4d5d37c8dab9f4b5f603f6313adaee3
viniciusrsouza <vinicius.rsouza07@gmail.com>
2019-07-14 21:14:21 -0300

    beta branch

--- src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
+++ src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
@@ -3,7 +3,9 @@ package supervinicius.bot
 object SuperBotLauncher {
     @JvmStatic
     fun main(args: Array<String>) {
-        val TOKEN = "NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4"
+        //    beta token NjAwMTE2NDc3NDQxNDc0NTYy.XSvEbg.hpqj_OYsWQ2am6nAr_OX75q1fZE
+        // release token NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4
+        val TOKEN = "NjAwMTE2NDc3NDQxNDc0NTYy.XSvEbg.hpqj_OYsWQ2am6nAr_OX75q1fZE"
         val bot = SuperBot(TOKEN)
 
         bot.start()
