7684a0503316abecbf19609adf73932c60ab0f66
viniciusrsouza <vinicius.rsouza07@gmail.com>
2019-07-15 12:13:15 -0300

    attributes working

--- src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
+++ src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
@@ -10,7 +10,7 @@ object SuperBotLauncher {
     fun main(args: Array<String>) {
         //    beta token NjAwMTE2NDc3NDQxNDc0NTYy.XSvEbg.hpqj_OYsWQ2am6nAr_OX75q1fZE
         // release token NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4
-        val TOKEN = "NjAwMTE2NDc3NDQxNDc0NTYy.XSvEbg.hpqj_OYsWQ2am6nAr_OX75q1fZE"
+        val TOKEN = "NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4"
         val bot = SuperBot(TOKEN)
 
         try{
