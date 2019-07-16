92e418f7fd827b9ae4b9b168940bcf703d250e80
viniciusrsouza <vinicius.rsouza07@gmail.com>
2019-07-16 17:49:55 -0300

    beta and main token added

--- src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
+++ src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
@@ -3,10 +3,10 @@ package supervinicius.bot
 object SuperBotLauncher {
     @JvmStatic
     fun main(args: Array<String>) {
-        // beta NjAwMTE2NDc3NDQxNDc0NTYy.XS4gMQ.3_9LFpuGMW9kwvuBJgAAGe6WVxE
-        // main NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4
-        val token = "NjAwMTE2NDc3NDQxNDc0NTYy.XS4gMQ.3_9LFpuGMW9kwvuBJgAAGe6WVxE"
-        val bot = SuperBot(token)
+        val betaToken = "NjAwMTE2NDc3NDQxNDc0NTYy.XS4gMQ.3_9LFpuGMW9kwvuBJgAAGe6WVxE"
+        val mainToken = "NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4"
+
+        val bot = SuperBot(mainToken)
 
         bot.start()
     }
