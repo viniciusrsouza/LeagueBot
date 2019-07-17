50bba88a04fc77cad0a20c76a275892c48f413c1
Vinícius Souza <vinicius.rsouza07@gmail.com>
2019-07-17 00:26:10 +0000

    fixing host

--- src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
+++ src/main/kotlin/supervinicius/bot/SuperBotLauncher.kt
@@ -6,7 +6,7 @@ object SuperBotLauncher {
         val betaToken = "NjAwMTE2NDc3NDQxNDc0NTYy.XS4gMQ.3_9LFpuGMW9kwvuBJgAAGe6WVxE"
         val mainToken = "NTk5NDIzMDkzNjg1MDkyMzcy.XSlAOA.0NcSqJpuszdjXm-5sAbBgCgVU_4"
 
-        val bot = SuperBot(betaToken)
+        val bot = SuperBot(mainToken)
 
         bot.start()
     }
